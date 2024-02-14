2aafe264ac5a2e95cff8cf6933797983541f36dd
abderrahmane-18 <abderrahmane.boukhezar@gmail.com>
2024-02-14 19:30:08 +0100

    add delete request

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResource('tasks',TaskController::class)->only(
-    ['index','show','store',]
-);
-//Route::get('tasks',[TaskController::class,'index']);
+Route::apiResource('tasks',TaskController::class);
+//Route::get('tasks',[TaskController::class,'index','update']);
